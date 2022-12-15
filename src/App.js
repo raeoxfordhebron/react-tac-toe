8b327620339343ffedf374bbb928ff5494cfb947
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-12-14 21:01:07 -0500

    Player and Header components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Heading from "./components/Heading"
+import Header from "./components/Header"
+import Player from "./components/Player"
+import Board from "./components/Board"
 
 function App(){
-  return (<h1>React Tac Toe</h1>)
+  return (<>
+  <Header/>
+  <Player/>
+  </>)
 }
 
 export default App;
